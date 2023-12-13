8fae368386dd5224c3a909968c2dbcbebd14d161
Vijay6868 <saroyav3@gmail.com>
2023-12-13 22:17:59 +1300

    class library added

--- schooldHRAdministration/schooldHRAdministration/Program.cs
+++ schooldHRAdministration/schooldHRAdministration/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HRAdministrationAPI;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,5 +12,9 @@ namespace schooldHRAdministration
         static void Main(string[] args)
         {
         }
+    }
+    public class Teacher: EmployeeBase
+    {
+
     }
 }
